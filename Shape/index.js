cb8ed7b3917aa5af5991bd3162d23c414ac105d0
Maynard246 <41711707+Maynard246@users.noreply.github.com>
2018-11-07 14:01:33 +0800

    Add files via upload

---
+++ Shape/index.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 950){
+            $(".list").css({"opacity" : "1"})
+        }
+ else {
+        $(".list").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1250){
+            $(".list2").css({"opacity" : "1"})
+        }
+ else {
+        $(".list2").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1650){
+            $(".studioimg").css({"opacity" : "1"})
+        }
+ else {
+        $(".studioimg").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1850){
+            $(".studiotxt li:nth-child(1)").css({"opacity" : "1"})
+        }
+ else {
+        $(".studiotxt li:nth-child(1)").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1850){
+            $(".studiotxt li:nth-child(2)").css({"opacity" : "1"})
+        }
+ else {
+        $(".studiotxt li:nth-child(2)").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1850){
+            $(".studiotxt li:nth-child(3)").css({"opacity" : "1"})
+        }
+ else {
+        $(".studiotxt li:nth-child(3)").css({"opacity" : "0"})
+    }
+ })
+ })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1850){
+            $(".studiotxt li:nth-child(4)").css({"opacity" : "1"})
+        }
+ else {
+        $(".studiotxt li:nth-child(4)").css({"opacity" : "0"})
+    }
+ })
+ })
